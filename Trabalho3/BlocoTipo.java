60771de908b19a76d6f850b0ad690305309ff646
Agnekarilayne <167125867+Agnekarilayne@users.noreply.github.com>
2025-06-20 17:37:27 -0300

    criação da classe BlocoTipo

---
+++ Trabalho3/BlocoTipo.java
@@ -0,0 +1,9 @@
+package Trabalho3;
+
+public class BlocoTipo {
+    public static int Fundo = 0;
+    public static int Bloco1 = 1;
+    public static int Bloco2 = 2;
+    public static int Bloco3 = 3;
+    public static int Bloco4 = 4;
+}
