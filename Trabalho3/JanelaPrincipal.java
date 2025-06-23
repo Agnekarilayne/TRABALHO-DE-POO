7a2628e01e633ec4f86435b67dc9289ba6bf3115
rhuan <162179904+Zoin1554k@users.noreply.github.com>
2025-06-22 21:39:53 -0300

    Update JanelaPrincipal.java
    Adicionei duas funções, uma para mover e construir e outra para desenhar a posição do avatar, ficou faltando só puxar as imagens.

--- Trabalho3/JanelaPrincipal.java
+++ Trabalho3/JanelaPrincipal.java
@@ -1,4 +1,3 @@
-package Trabalho3;
 
 import javax.swing.*;
 import java.awt.*;
@@ -6,26 +5,87 @@ import java.awt.*;
 public class JanelaPrincipal extends JFrame {
     private  int LINHAS = 20;
     private  int COLUNAS = 20;
-    private  int TOTAL = LINHAS * COLUNAS; //20x20	
+    private  int TOTAL = LINHAS * COLUNAS; //20x20
 
     private MyPanel[] blocos = new MyPanel[TOTAL]; //vetor 400 posiçoes, cada um com mypanel individual -> um bloco
 
-    private ImageIcon fundoIcon = new ImageIcon(getClass().getResource("/imagensdotrabalho/fundo.png")); //carrega e busca a imagem da pasta
+    private ImageIcon fundoIcon = new ImageIcon(getClass().getResource("")); //carrega e busca a imagem da pasta
 
+    private int posAv = 0;
+    private int[] tiposBlocos = new int[TOTAL]; // guarda tipo de cada bloco
+    private ImageIcon avatarIcon = new ImageIcon(getClass().getResource(""));
+    private ImageIcon[] imagensBlocos = {
+            new ImageIcon(getClass().getResource("")), // 0
+            new ImageIcon(getClass().getResource("")), // 1
+            new ImageIcon(getClass().getResource("")), // 2
+            new ImageIcon(getClass().getResource("")), // 3
+            new ImageIcon(getClass().getResource(""))  // 4
+    };
+    private void desenharAvatar() {
+        for (int i = 0; i < TOTAL; i++) {
+            blocos[i].setIcon(imagensBlocos[tiposBlocos[i]]);
+        }
+        blocos[posAv].setIcon(avatarIcon); // desenha avatar na posição atual
+    }
+    private void moverOuConstruir(java.awt.event.KeyEvent e) { //metodo para mover e colocar peça
+        int key = e.getKeyCode();
+        switch (key) {
+            case java.awt.event.KeyEvent.VK_LEFT:
+                if (posAv % COLUNAS != 0) posAv--;
+                break;
+            case java.awt.event.KeyEvent.VK_RIGHT:
+                if (posAv % COLUNAS != COLUNAS - 1) posAv++;
+                break;
+            case java.awt.event.KeyEvent.VK_UP:
+                if (posAv >= COLUNAS) posAv -= COLUNAS;
+                break;
+            case java.awt.event.KeyEvent.VK_DOWN:
+                if (posAv < TOTAL - COLUNAS) posAv += COLUNAS;
+                break;
+            case java.awt.event.KeyEvent.VK_1:
+                tiposBlocos[posAv] = BlocoTipo.Bloco1;
+                break;
+            case java.awt.event.KeyEvent.VK_2:
+                tiposBlocos[posAv] = BlocoTipo.Bloco2;
+                break;
+            case java.awt.event.KeyEvent.VK_3:
+                tiposBlocos[posAv] = BlocoTipo.Bloco3;
+                break;
+            case java.awt.event.KeyEvent.VK_4:
+                tiposBlocos[posAv] = BlocoTipo.Bloco4;
+                break;
+            case java.awt.event.KeyEvent.VK_SPACE:
+                tiposBlocos[posAv] = BlocoTipo.Fundo;
+                break;
+        }
+        desenharAvatar();
+    }
 
     public JanelaPrincipal() {
         setTitle("Minecraft");
         setSize(640, 640); //tamanho da janela
         setDefaultCloseOperation(EXIT_ON_CLOSE); //fecha quando clica no x
 
-        setLayout(new GridLayout(LINHAS, COLUNAS)); //grade regular 
+        setLayout(new GridLayout(LINHAS, COLUNAS)); //grade regular
 
         for (int i = 0; i < TOTAL; i++) {
+            tiposBlocos[i] = BlocoTipo.Fundo;
             blocos[i] = new MyPanel(fundoIcon);
             add(blocos[i]);
         } //loop para criar e adicionar os 400 painel
 
+        desenharAvatar();//chamando a função desenhar avatar
+
+        addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent e) {
+                moverOuConstruir(e);
+            }
+        });
+
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
         setVisible(true); //torna visivel a janela
+
     }
 
     public static void main(String[] args) {
