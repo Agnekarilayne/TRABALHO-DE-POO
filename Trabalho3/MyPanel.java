8b264f4786afd06934e54a8fa7eac224a1aafd64
Anthony Prado <167304421+Arpa4yMaracas@users.noreply.github.com>
2025-06-23 20:18:11 -0300

    Update MyPanel.java

--- Trabalho3/MyPanel.java
+++ Trabalho3/MyPanel.java
@@ -1,14 +1,19 @@
-package Trabalho3;
-
 import javax.swing.JPanel;
 import javax.swing.ImageIcon;
 import java.awt.Graphics;
+import java.awt.Dimension;
 
 public class MyPanel extends JPanel {
     private ImageIcon icon; //declara atributo
 
     public MyPanel(ImageIcon icon) {
         this.icon = icon;
+        setPreferredSize(new Dimension(32, 32)); // define tamanho fixo para cada bloco
+        setMaximumSize(new Dimension(32, 32));   // tamanho máximo
+        setMinimumSize(new Dimension(32, 32));   // tamanho mínimo
+        setOpaque(false); // remove o fundo branco do JPanel
+        setBorder(null);  // sem borda
+        setLayout(null);  // remove layout padrão
     } //construtor da classe
 
     public void setIcon(ImageIcon icon) {
@@ -19,7 +24,6 @@ public class MyPanel extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g); //metodo do JPanel que garante que a tela fique limpa antes de desenhar algo novo
-
 
         if (icon != null) {
             g.drawImage(icon.getImage(), 0, 0, getWidth(), getHeight(), null); //desenho da imagem,coordenadas onde começa, altura e largura(...)
